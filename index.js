9479ea617e426a07299458abfa89b4184f76f428
neoboi76 <dattimbol@mymail.mapua.edu.ph>
2023-07-26 18:14:46 +0800

    Add main files (2)

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-
+const inputElement = document.querySelector('.js-task-input');
+const dateElement = document.querySelector('.js-task-date');
 let toDoList = JSON.parse(localStorage.getItem('listItems')) ||[{
   task:'',
   duedate: '',
+}];
+
+let historyList = [{
+  tasked: '',
+  dateFinished: '',
 }];
 
 renderList();
@@ -13,18 +19,17 @@ function renderList() {
     const { task, duedate } = listObject;
 
     let html = `
-    
     <div>${task}</div>
     <div>${duedate}</div>
     <button class="js-delete-btn">Delete</button>
-    
+    <button class="js-done-btn">Done</button>  
     `;
     listHTML += html
+    
     document.querySelector('.js-field').classList.add('todo-grid');
   });
   document.querySelector('.js-field').innerHTML = listHTML;
   
-
   document.querySelectorAll('.js-delete-btn').forEach((deleteElement, index) => {
     deleteElement.addEventListener('click', () => {
       toDoList.splice(index, 1)
@@ -36,14 +41,24 @@ function renderList() {
 }
 
 document.querySelector('.js-add-btn').addEventListener('click', () => {
-  addItem();
+  if (inputElement.value === '') {
+    return;
+  } else {
+    addItem();
+  }
 });
 
-
+document.body.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    if (inputElement.value === '') {
+      return;
+    } else {
+      addItem();
+    }
+  }
+});
 
 function addItem() {
-  const inputElement = document.querySelector('.js-task-input');
-  const dateElement = document.querySelector('.js-task-date');
   const task = inputElement.value;
   const duedate = dateElement.value;
 
@@ -57,5 +72,6 @@ function addItem() {
   renderList();
 
   localStorage.setItem('listItems', JSON.stringify(toDoList));
-
 }
+
+
